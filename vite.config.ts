aaa602d8b13359a552fbe89697bb088dae588e68
Fei Xie <feixie1980@yahoo.com>
2025-02-07 16:52:00 -0800

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/coding-ai-chat',
   plugins: [
     react({
       plugins: [['@swc/plugin-styled-components', {}]],
