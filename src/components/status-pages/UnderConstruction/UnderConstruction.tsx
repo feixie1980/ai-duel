c89a23d1293a24bd5d655114023da5c9ad4f7431
Fei Xie <feixie1980@yahoo.com>
2025-02-17 21:50:26 -0800

    fix: added loading spinner at the beginning

--- src/components/status-pages/UnderConstruction/UnderConstruction.tsx
+++ src/components/status-pages/UnderConstruction/UnderConstruction.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { UnderConstructionProps } from './UnderConstruction.types';
 import UnderConstructionImg from '../../../assets/under_construction_beach.png';
 import { createGlobalStyle } from 'styled-components';
 import DeliusUnicaseBold from '../../../assets/fonts/DeliusUnicase-Bold.ttf';
+import { Spinner } from '@radix-ui/themes';
 
 const FontStyle = createGlobalStyle`
   @font-face {
@@ -27,13 +28,47 @@ const UCImage = styled.img`
   width: 50%;
 `;
 
+const SpinnerOverlay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
+
 const UnderConstruction: React.FC = ({ className }: UnderConstructionProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (imgRef.current?.complete) {
+      setIsLoading(false);
+    }
+  }, [imgRef]);
+
   return (
     <>
       <FontStyle />
       <StyledWrapper className={className}>
-        <h1>Under construction, coming soon!</h1>
-        <UCImage src={UnderConstructionImg} />
+        <>
+          <h1>Under construction, coming soon!</h1>
+          <UCImage
+            ref={imgRef}
+            src={UnderConstructionImg}
+            onLoad={() => {
+              setIsLoading(false);
+            }}
+          />
+        </>
+        {isLoading && (
+          <SpinnerOverlay>
+            <Spinner size="3" />
+          </SpinnerOverlay>
+        )}
       </StyledWrapper>
     </>
   );
